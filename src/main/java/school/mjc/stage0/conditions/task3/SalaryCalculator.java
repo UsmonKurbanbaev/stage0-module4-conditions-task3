050bf2246ba9e1a41b5bdb657b3d0efdda19e23b
Usmon Kurbanbaev <usmonkurbanbaev1@gmail.com>
2023-01-07 21:55:11 +0500

    for conditional task3

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if(salary < 0){
+            System.out.println("wrong input!");
+        }
+        else {
+            if (salary <= 10000) {
+                System.out.println(salary * 0.85);
+            }
+            if (10000 < salary && salary <= 20000) {
+                System.out.println(salary * 0.82);
+            }
+            if (salary > 20000) {
+                System.out.println(salary * 0.8);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SalaryCalculator calculator = new SalaryCalculator();
+
+        calculator.calculateSalary(5000);
     }
 }
+
+
+
